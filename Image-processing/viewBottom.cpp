cfe4d4dd6909adf1c762d1fe094ab786508e5142
hugom <hugo.mathh@gmail.com>
2019-05-31 13:52:19 +0200

    bottom()

--- Image-processing/viewBottom.cpp
+++ Image-processing/viewBottom.cpp
@@ -8,8 +8,11 @@ ViewBottom::ViewBottom(Controller& _controller) : controller(_controller)
 
 void ViewBottom::display() const
 {
-	printY("\t6 - Open an other image");
-	printY("\t7 - Display your image");
+	std::cout << std::endl << std::endl;
+	printY("\t|----------Image settings---------|");
+	std::cout << std::endl;
+	printB("\t6 - Open an other image");
+	printB("\t7 - Display your image");
 	printG("\t8 - Restore your image");
 	printG("\t9 - Save current image");
 	printRB("\t10 - Exit");
